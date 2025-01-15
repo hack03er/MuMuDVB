a7f555b7d8939b623c1b410a3b26f8934e6f3a17
Johannes Hack <contact@johanneshack.de>
2025-01-15 18:14:52 +0100

    general table_needs_update

--- src/rewrite.h
+++ src/rewrite.h
@@ -151,4 +151,5 @@ void eit_rewrite_new_global_packet(unsigned char *ts_packet, rewrite_parameters_
 void eit_rewrite_new_channel_packet(unsigned char *ts_packet, rewrite_parameters_t *rewrite_vars, mumudvb_channel_t *channel,
 		unicast_parameters_t *unicast_vars, void *scam_vars_v);
 
+bool table_needs_update(char *mod_log_module, const int stored_version, unsigned char *buf);
 #endif
